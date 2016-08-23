7f2986c84e39c0f147e895fe69614ff9e87567a7
fengtingting <fengtingting@cecb2b.com>
2016-08-23 13:59:09 +0800

    解决方案最终

--- js/base.js
+++ js/base.js
@@ -125,6 +125,34 @@
             var $thisfnum = $this.attr("floornums");
             $('html,body').animate({scrollTop: $(".floorflag[floornums='"+$thisfnum+"']").offset().top}, 200)
         })
+        //ie8的placeholder属性
+         $(function() {
+             if( !('placeholder' in document.createElement('input')) ){   
+
+            $('input[placeholder],textarea[placeholder]').each(function(){    
+              var that = $(this),    
+              text= that.attr('placeholder');    
+              if(that.val()===""){    
+                that.val(text).addClass('placeholder');    
+              }    
+              that.focus(function(){    
+                if(that.val()===text){    
+                  that.val("").removeClass('placeholder');    
+                }    
+              })    
+              .blur(function(){    
+                if(that.val()===""){    
+                  that.val(text).addClass('placeholder');    
+                }    
+              })    
+              .closest('form').submit(function(){    
+                if(that.val() === text){    
+                  that.val('');    
+                }    
+              });    
+            });    
+          }   
+   })
     function restore(c_x){
     if(c_x){
         var numc_x = parseInt(c_x);
